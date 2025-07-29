86af872ee6c91da06d9c34d72101cc3e17eab264
Artur Abrao <arturmabrao@gmail.com>
2025-07-29 13:43:56 -0300

    Ajustes

--- pyB.py
+++ pyB.py
@@ -2,10 +2,10 @@
 
 e = ''
 operation = 'I'
-balance, limit_w, withdraw_value, deposite_value, = 0, 0, 0, 1500
+balance, count_w, limit_w, withdraw_value, deposite_value, = 0, 0, 500, 0, 1500
 
-while operation > 'F' :
-    operation = input("Operacoes:\n" 
+while operation != 'F':
+    operation = input("\n\nOperacoes:\n" 
     "C - Consultar saldo\n" \
     "D - Depositar\n" \
     "S - Sacar\n" \
@@ -15,37 +15,40 @@ while operation > 'F' :
     if operation == 'F':
         break
     elif operation == 'C':
-        print(f'Valor do seu saldo: {balance}')
+        print(f'\nValor do seu saldo: {balance}')
     elif operation == 'S':
-        withdraw_value = input("Qual valor deseja sacar?")
+        if count_w > 2:
+            print("\nVoce ja realizou 3 saques no dia de hoje")
+            continue
+        withdraw_value = float(input("Qual valor deseja sacar? "))
         if withdraw_value < 0:
-            print("O valor de um saque deve ser positivo")
+            print("\nO valor de um saque deve ser positivo")
             continue
         elif withdraw_value > balance:
-            print(f'Voce nao pode sacar {withdraw_value}, pois seu saldo e de {balance}')
+            print(f'\nVoce nao pode sacar {withdraw_value}, pois seu saldo e de {balance}')
             continue
         elif withdraw_value > limit_w:
-            print(f'Voce nao pode sacar {withdraw_value} pois e maior que seu limite de saque disponivel {limit_w}')
+            print(f'\nVoce nao pode sacar {withdraw_value} pois seu limite de valor por saque {limit_w}')
         else:
             try:
                 balance-=withdraw_value
-                limit-=withdraw_value
-                print(f'Apos o saque, o valor do seu saldo e: {balance}')
+                count_w+=1
+                print(f'\nApos o saque, o valor do seu saldo e: {balance}')
             except e:
                 print(e)
     elif operation == 'D':
-        deposite_value = input("Qual valor deseja depositar?")
+        deposite_value = float(input("\nQual valor deseja depositar? "))
         if deposite_value < 0:
-            print("O valor de um deposito ser positivo")
+            print("\nO valor de um deposito ser positivo")
             continue
         else:
             try:
                 balance+=deposite_value
-                print(f'Apos o deposito, o valor do seu saldo e: {balance}')
+                print(f'\nApos o deposito, o valor do seu saldo e: {balance}')
             except e:
                 print (e)            
 
 
 #    operation = 'F'
 
-print("Finalizado.\nAté a próxima.")
+print("\nOperacao Finalizada.\nAté a próxima.")
